974f749857811149683357a0b3d2e6280d7bbbb6
Varenicou <varenicou@gmail.com>
2024-11-29 15:08:27 +0700

    Test Commit

---
+++ Tugas_Test_EID/Tugas_Test_EID/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tugas_Test_EID.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
